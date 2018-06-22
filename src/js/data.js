44a9761895192898f1559e721617529ee1b4c926
Valeria Cossio <valeriacossio5@gmail.com>
2018-06-22 01:26:02 -0400

    pushpush

--- src/js/data.js
+++ src/js/data.js
@@ -1,38 +1,62 @@
-Promise.all([    //Ejecuta todas las llamadas de manera paralela
-        fetch('/data/cohorts/lim-2018-03-pre-core-pw/users.json'),
-        fetch('/data/cohorts/lim-2018-03-pre-core-pw/progress.json'),
-        fetch('/data/cohorts.json')
-]).then((responses)=>{   //Responde a todas las promesas
-    return Promise.all(responses.map((response => response.json())));
-}).then((responseJsons)=>{ //Arreglo de respuestas en json
-         const users = responseJsons[0];
-         const progress = responseJsons[1];
-         const cohorts = responseJsons[2];
-         console.log(users);
-         console.log(progress);
-         users.forEach(element => {
-             console.log(element.name);
-             console.log(element.role);
-             console.log(element.id);
-         });
-//hacer for o forEach para seleccionar un id o name, lo que sea...
-    
-         
+/*coursesData = {};
 
-        
-         //
-         // Código que ocupa los jsons...
-         //
-    }).catch(
-        (error)=>{ // Al menos una llamada falló
+Promise.all([ // Ejecuta todas las llamadas de manera paralela.
+  fetch('/data/cohorts/lim-2018-03-pre-core-pw/users.json'),
+  fetch('/data/cohorts/lim-2018-03-pre-core-pw/progress.json'),
+  fetch('/data/cohorts.json')
+]).then((responses)=>{ // Responde a todas las promesas.
+  return Promise.all(responses.map((response => response.json())));
+}).then((responseJsons)=>{ // Arreglo de respuestas en json.
+  let users = responseJsons[0];
+  let progress = responseJsons[1];
+  let courses = responseJsons[2];
+
+  var ids = courses.map(function (courses) {
+     return courses.id;
+     console.log(ids);
+    });
+  
+
+  
+  computeUsersStats(users, progress, courses);
+}).catch(
+  (error)=>{ // Al menos una llamada falló.
+  }
+);
+
+/*window.computeUsersStats = (users, progress, courses) => {
+ let esto = users.map(
+   (user) => {
+     if (Object.keys(progress[user.id]).length ===0) {
+       return user
+     }  
+          user.stats: {
+            percent: percent,
+            exercices: {
+              total: practiceTotal,
+              completed: practiceCompleted,
+              percent: Math.round((practiceCompletes / practiceTotal)*100),
+            },
+            reads: {
+              total: readsTotal,
+              completed:readsCompleted,
+              percent: Math.round((readsCompletes / readsTotal)*100),
+            },
+            quizzes: {
+              total: quizzTotal,
+              completed: quizzCompleted,
+              percent: Math.round((quizzCompleted / quizzTotal)*100),
+              scoreSum: scoreSumQuizz,
+              scoreAvg: Math.round(scoreSumQuizz / quizzCompleted),
+            }
+          }
+          return user;
         }
-    );
-
-   
-   
-
-   
+      }
+    )
+  }  
+};*/
 
 
 
-    
+ 
---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+function lima() {
+    document.write("Hola");
+}
+
+function mexico() {
+    document.write("Aquí");
+}
+
+function brasil() {
+    document.write("brasiul")
+}
+
+function chile() {
+    document.write("chili")
+}
+   
+
