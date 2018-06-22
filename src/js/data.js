db93d6e41726a5519c2d3618e16da2bd13702167
Valeria Cossio <valeriacossio5@gmail.com>
2018-06-22 17:06:44 -0400

    funcion 1

--- src/js/data.js
+++ src/js/data.js
@@ -1,5 +1,3 @@
-/*coursesData = {};
-
 Promise.all([ // Ejecuta todas las llamadas de manera paralela.
   fetch('/data/cohorts/lim-2018-03-pre-core-pw/users.json'),
   fetch('/data/cohorts/lim-2018-03-pre-core-pw/progress.json'),
@@ -7,56 +5,119 @@ Promise.all([ // Ejecuta todas las llamadas de manera paralela.
 ]).then((responses)=>{ // Responde a todas las promesas.
   return Promise.all(responses.map((response => response.json())));
 }).then((responseJsons)=>{ // Arreglo de respuestas en json.
+
   let users = responseJsons[0];
   let progress = responseJsons[1];
   let courses = responseJsons[2];
 
-  var ids = courses.map(function (courses) {
-     return courses.id;
-     console.log(ids);
-    });
-  
-
-  
-  computeUsersStats(users, progress, courses);
+ computeUsersStats(users, progress, courses);
+ 
 }).catch(
   (error)=>{ // Al menos una llamada falló.
   }
 );
 
-/*window.computeUsersStats = (users, progress, courses) => {
- let esto = users.map(
-   (user) => {
-     if (Object.keys(progress[user.id]).length ===0) {
-       return user
-     }  
-          user.stats: {
-            percent: percent,
-            exercices: {
-              total: practiceTotal,
-              completed: practiceCompleted,
-              percent: Math.round((practiceCompletes / practiceTotal)*100),
-            },
-            reads: {
-              total: readsTotal,
-              completed:readsCompleted,
-              percent: Math.round((readsCompletes / readsTotal)*100),
-            },
-            quizzes: {
-              total: quizzTotal,
-              completed: quizzCompleted,
-              percent: Math.round((quizzCompleted / quizzTotal)*100),
-              scoreSum: scoreSumQuizz,
-              scoreAvg: Math.round(scoreSumQuizz / quizzCompleted),
+
+window.computeUsersStats = (users, progress, courses) => {
+  users.map(user => {
+    courses.map(course => {     
+      if (progress[user.id][course]) {        
+        user.stats = {
+          percent: progress[user.id][course].percent,
+          exercises: {
+            total: 0,
+            completed: 0
+          },
+          quizzes: {
+            total: 0,
+            completed: 0,
+            scoreSum: 0
+          },
+          reads: {
+            total: 0,
+            completed: 0
+          },
+        };
+
+        // Recupera los valores del objeto y los devuelve en un arreglo
+        Object.values(progress[user.id][course].units).forEach(unit => {
+          Object.values(unit.elements).forEach(element => {
+            switch (element) {             
+            case element.type === 'practice':          
+              user.stats.exercises.total++;
+              user.stats.exercises.completed.push(element.completed);
+            
+            case element.type === 'quiz': 
+              user.stats.quizzes.total++;
+              user.stats.quizzes.completed.push(element.completed);
+            
+            case element.completed: 
+              user.stats.quizzes.scoreSum.push(element.score);
+              
+            case element.type === 'read':
+              user.stats.reads.total++;
+              user.stats.reads.completed.push(element.completed);
             }
           }
-          return user;
-        }
-      }
-    )
-  }  
-};*/
+          );
+        });
 
+        // Promedio porcentaje
+        user.stats.exercises.percent = Math.round(user.stats.exercises.completed / user.stats.exercises.total * 100);
+        user.stats.quizzes.percent = Math.round(user.stats.quizzes.completed / user.stats.quizzes.total * 100);
+        user.stats.quizzes.scoreAvg = Math.round(user.stats.quizzes.scoreSum / user.stats.quizzes.completed);
+        user.stats.reads.percent = Math.round(user.stats.reads.completed / user.stats.reads.total * 100);
+      } else {
+        user.stats = {
+          percent: 0,
+          exercises: {},
+          quizzes: {},
+          reads: {},
+        };
+      }
+    });
+  });
+  return users;
+};
+
+
+window.sortUsers = (users, orderBy, orderDirection) => {
+  if (orderBy === 'name') {
+    return users.sort(orderByName(orderDirection));
+  } else if (orderBy === 'stats.percent') {
+    return users.sort(orderByTotalPercentage(orderDirection));
+  } else {
+    return users.sort(orderByStats(orderBy, orderDirection));
+  }
+};
+
+window.orderByName = (orderDirection) => {
+  return function(student1, student2) {
+    let comparisonResult = student1.name.localeCompare(student2.name);
+    return orderDirection === 'ASC' ? comparisonResult : -comparisonResult;
+  };
+};
+
+window.orderByTotalPercentage = (orderDirection) => {
+  return function(student1, student2) {
+    let comparisonResult = student1.stats.percent - student2.stats.percent;
+    return orderDirection === 'ASC' ? comparisonResult : -comparisonResult;
+  };
+};
+
+window.orderByStats = (orderBy, orderDirection) => {
+  let criteria = orderBy.split('.');
+  return function(student1, student2) {
+    let comparisonResult = student1.stats[quizzes[1]][completed[2]] - student2.stats[quizzes[1]][completed[2]];
+    return orderDirection === 'ASC' ? comparisonResult : -comparisonResult;
+  };
+};
+
+window.filterUsers = (users, search) => {
+  let upperCaseSearch = search.toUpperCase();
+  return users.filter(user => user.name.toUpperCase().includes(upperCaseSearch));
+};
+console.log(window, filterUsers);
 
 
  
