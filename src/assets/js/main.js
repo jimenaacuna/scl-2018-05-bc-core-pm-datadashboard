ca8cebea39e201b8cbe2fc6759bad119042526ab
jimenaacuna <jimena.a.saavedra@gmail.com>
2018-06-24 19:58:44 -0400

    arreglos

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 muestraCohorts = (cohorts) => {
   const cohortsId = cohorts.map(elemento => elemento.id);
   const contenedor = document.getElementById('cohortsData');
@@ -8,5 +9,27 @@ muestraCohorts = (cohorts) => {
     console.log()
   }); 
 };
+=======
+// Función de display navbar
+$('.btn-expand-collapse').click(function(e) {
+  $('.navbar-primary').toggleClass('collapsed');
+});
+  
+  
+var delay = 500;
+$('.progress-bar').each(function(i) {
+  $(this).delay(delay * i).animate({ width: $(this).attr('aria-valuenow') + '%' }, delay);
+  
+  $(this).prop('Counter', 0).animate({
+    Counter: $(this).text()
+  }, {
+    duration: delay,
+    easing: 'swing',
+    step: function(now) {
+      $(this).text(Math.ceil(now) + '%');
+    }
+  });
+});
+>>>>>>> arreglos
 
 
